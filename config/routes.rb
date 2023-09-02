aa2929d262cf5a3532d05bfee13d144ced93a74c
Tom Williams <iamtom87@hotmail.com>
2023-09-02 17:52:17 +0100

    changed schema, updated models, now join table for venue_meetups

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'meetups/show'
+  get 'meetups/new'
+  get 'meetups/index'
+  get 'meetups/create'
+  get 'meetups/update'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
