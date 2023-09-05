40c25baf05f145ff9b9fa4f5da80f5e3db462f19
Tom Williams <iamtom87@hotmail.com>
2023-09-05 21:34:57 +0100

    meetups controller create and index including some basic views for these methods

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,17 +1,40 @@
 class MeetupsController < ApplicationController
+  # before_action :set_user
+
   def show
-    @meetup = Meetup.find
+    @meetup = Meetup.find(params[:id])
   end
 
   def new
+    @meetup = Meetup.new
   end
 
   def index
-  end
+    @meetups = current_user.meetups
+    end
 
   def create
+    @meetup = Meetup.new(meetup_params)
+    @meetup.passcode = passcode
+    @meetup.user = current_user
+    if @meetup.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def update
+
+  private
+
+  def meetup_params
+    params.require(:meetup).permit(:name, :event_date, :purpose, :location)
+  end
+
+  def passcode(length = 5) 
+    # create password, 5 digit rand numbers
+    numbers = (0..9).to_a
+    num_array = numbers.sample(length)
+    num_array.join
   end
 end
