82762d388b09902e5676523fb9fec7d83513acf8
Tom Williams <iamtom87@hotmail.com>
2023-09-12 20:33:43 +0100

    added to seed file

---
+++ config/initializers/global_data.rb
@@ -0,0 +1,5 @@
+GLOBAL_RESTAURANTS = ["The Phoenix, Cuisine: British Pub Food Address: 23 Smith St, Chelsea, London SW3 4EE, Distance: Approximately 0.2 miles away",
+                      "The Good Life Eatery, Cuisine: Healthy, Vegetarian-Friendly, Address: 59 Sloane Ave, Chelsea, London SW3 3DH, Distance: Approximately 0.4 miles away",
+                      "Kurobuta Chelsea, Cuisine: Japanese, Asian Fusion, Address: 312 King's Rd, Chelsea, London SW3 5UH, Distance: Approximately 0.3 miles away",
+                      "Medlar, Cuisine: Modern European, Address: 438 King's Rd, Chelsea, London SW10 0LJ, Distance: Approximately 0.4 miles away",
+                      "Aubaine, Cuisine: French, European, Address: 260-262 Brompton Rd, Knightsbridge, London SW3 2AS, Distance: Approximately 0.4 miles away"]
