11b74c8249256c4236939a91cae2f0aabb32c9b4
David Hickson <davidedwardhickson@gmail.com>
2023-09-16 10:33:16 +0100

    create button from home page plus

--- config/routes.rb
+++ config/routes.rb
@@ -23,12 +23,4 @@ Rails.application.routes.draw do
       post 'create_chatroom'
     end
   end
-  # resources :meetups do
-  #   resources :user_meetups
-  #   resource :chatroom, only: [:show, :destroy] do
-  #     post 'create', on: :member  # Use 'post' instead of 'create'
-  #   end
-
-  #   resources :messages, only: :create, module: :chatrooms
-  # end
 end
