f2ace2c5eb9cb3bbf203bf8c1e9bdb543e1d610d
davidavidavidavidavidave <128969589+davidavidavidavidavidave@users.noreply.github.com>
2023-09-26 19:09:46 +0100

    chatroom styled

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,37 +1,41 @@
-import { Controller } from "@hotwired/stimulus"
-import { createConsumer } from "@rails/actioncable"
+import { Controller } from "@hotwired/stimulus";
+import { createConsumer } from "@rails/actioncable";
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
-  static targets = ["messages"]
+  static targets = ["messages"];
   static values = {
     chatroomId: Number,
     currentUserId: Number, // Define a value for current user ID
-  }
+  };
 
   connect() {
-    console.log(`connecting to ActionCable channel with id ${this.chatroomIdValue}`)
-    console.log(`Current User ID: ${this.currentUserIdValue}`)
-
     createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => { this.insertMessage(data) } }
+      {
+        received: (data) => {
+          this.insertMessage(data);
+        },
+      }
     );
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
   }
 
   resetForm(event) {
-    event.target.reset()
+    event.target.reset();
   }
 
   insertMessage(data) {
     // Parse the received data as HTML
     const parser = new DOMParser();
-    const message = parser.parseFromString(data, 'text/html').body.firstChild;
+    const message = parser.parseFromString(data, "text/html").body.firstChild;
 
     // Add a class to the message based on user ID
-    if (message.classList.contains('msg')) {
-      const userId = message.getAttribute('data-user-id');
-      message.classList.add(userId == this.currentUser ? 'left-msg' : 'right-msg');
+    if (message.classList.contains("msg")) {
+      const userIdFromMessage = message.getAttribute("data-user-id");
+      message.classList.add(
+        userIdFromMessage == this.currentUser ? "left-msg" : "right-msg"
+      );
     }
 
     // Append the modified message to the messages container
@@ -44,6 +48,6 @@ export default class extends Controller {
   get currentUser() {
     // You should implement a way to get the current user's ID here
     // This is just a placeholder, and you should replace it with your logic
-    return parseInt(this.element.getAttribute('data-current-user-id'));
+    return parseInt(this.element.getAttribute("data-current-user-id"));
   }
 }
