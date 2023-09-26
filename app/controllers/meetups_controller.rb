baa841f7541790f7efb12799c602f51f1f37651c
Tom Williams <iamtom87@hotmail.com>
2023-09-26 12:50:25 +0100

    added suggested places array and sampling 5

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -3,15 +3,7 @@ class MeetupsController < ApplicationController
 
 
   def show
-    @global_restaurants = ["Brasserie Zédel - Classic French brasserie located in a stunning Art Deco setting. 20 Sherwood St, Soho, London W1F 7ED",
-                           "Dishoom - Popular Indian restaurant inspired by the Irani cafés of Bombay. 5 St Martin's Ln London WC2N 4AP",
-                           "Hawksmoor Air Street - Steakhouse known for its premium quality steaks and seafood. 5A Air St, London W1J 0AD",
-                           "Duck & Rice - Modern Chinese gastropub that combines classic Cantonese cuisine with a pub-style setting. 90 Berwick St, Soho, London W1F 0QB",
-                           "Barrafina Dean Street - Spanish tapas restaurant that offers a range of authentic and flavorful dishes. 26-27 Dean St, Soho, London W1D 3LL",
-                           "Polpo Soho - Venetian-inspired restaurant known for its small plates and relaxed ambiance. 41 Beak St, Soho, London W1F 9SB",
-                           "Honest Burgers - Known for its delicious, locally-sourced burgers made with high-quality ingredients. 4 Market Pl, London W1W 8AD",
-                           "The Riding House Café - Brasserie offering a diverse menu of British and European dishes, 43-51 Great Titchfield St, Fitzrovia, London W1W 7PQ"]
-
+    @suggested_places = generate_suggested
     @meetup = Meetup.find(params[:id])
     @users = @meetup.users
     @coordinates = user_coordinates
@@ -125,5 +117,38 @@ class MeetupsController < ApplicationController
 
   def geocode_centre
     Geocoder::Calculations.geographic_center(@coordinates)
+  end
+
+  def generate_suggested
+    places_array = ["Scarlet - Sultry 1920s inspired cocktail bar with live jazz and indulgent small plates.",
+                    "The Potting Shed - Rustic eatery set in a Victorian venue serving fresh garden-to-table British cuisine.",
+                    "Petit Paris - Charming French bistro with checkerboard floors and strings of fairy lights.",
+                    "East India Club - Colonial India inspired dining room featuring curries, tandoors and gin fizzes.",
+                    "Dolce & Salato - Neighborhood Italian highlighting handmade pastas and wood-fired pizzas.",
+                    "Old Tom's Pub - Classic British gastropub,fish & chips and bangers & mash with draft ales",
+                    "Wilkes & Co - Sophisticated steakhouse in a converted 18th century townhouse.",
+                    "Silk Road - Vibrant Turkish and Lebanese small plates meant for sharing.",
+                    "Franklin & Sons - Jewish deli crafting hand sliced sandwiches, bagels and smoked fish.",
+                    "The Copper Still - Whisky and Scotch flights paired with modern Scottish fare.",
+                    "Platform 9 3/4 - Hogwarts-themed restaurant serving dishes inspired by the wizarding world.",
+                    "The Upside Down - Demogorgon servers and food inspired by the horrors of the alternate dimension.",
+                    "The Slaughtered Lamb - Ancient pub with foreboding wooden beams and pints of thick ale. Beware the werewolves",
+                    "Luna Dorada - Colorful, zesty Mexican cantina with tequila samplers and live mariachi music on weekends.",
+                    "Signor Verdi - Italian trattoria crafting roast meats, pastas and pizzas in a wood-burning oven.",
+                    "Fat Tuesday's - New Orleans inspired Cajun and Creole cuisine like jambalaya, gumbo and crawfish boils.",
+                    "Tsubaki Sushi - Elegant and modern sushi spot featuring rare fish flown in daily.",
+                    "Chez Antoinette - Classic French fine dining in a baroque mirrored dining room.",
+                    "The Huntsman - Classic British gastropub with hearty roasts, meat pies and bread puddings.",
+                    "Black Truffle - Underground speakeasy with molecular gastronomy menu.",
+                    "221B Baker Street - Immersive Sherlock Holmes themed restaurant with mystery-inspired dishes and drinks.",
+                    "Cau - South American steakhouse with rodizio style grilled meats.",
+                    "Two Fold - Contemporary pizza, interesting pizza toppings.",
+                    "Casa Cuba - Cuban cuisine, buzzing eatery with mojitos and Latin music.",
+                    "The Brewers Inn - Traditional British pub with a restaurant that serves classic pub fare.",
+                    "Hood - Plant-based comfort food serving vegan versions of classic dishes.",
+                    "Berenjak - Vibrant Persian restaurant inspired by the bustling bazaars of Tehran.",
+                    "ClipClop - Wine bar focusing on natural and organic wines."]
+
+    places_array.sample(5)
   end
 end
